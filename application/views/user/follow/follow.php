ddc3d0be7b20818eebfa38f3ddfcd90c83b5c079
吕运 <yunlxyz@163.com>
2016-03-24 23:58:59 +0800

    开发 我的收藏页面开发

---
+++ application/views/user/follow/follow.php
@@ -0,0 +1,69 @@
+<!-- contents -->
+<div class="row" style="margin-top:5%;">
+  <div class="col-md-1"></div>
+  <div class="col-md-10 offset-body">
+    <div class="row">
+      <div class="col-md-9">
+        <div class="row">
+          <div class="col-md-12 clearfix">
+            <h4 class="follow-title">我关注的问题 (共七个)</h4>
+            <div class="follow-section-item">
+              <span class="follow-vote-count pull-left">
+                <div class="follow-vote-num">123</div>
+                <div class="follow-vote-type">浏览</div>
+              </span>
+              <div class="follow-section-main">
+                <h5 class="follow-question"><a href="#">去哪可以买到不贵又体面的家居装饰画呢？</a></h5>
+                <div class="follow-meta">
+                  <a href="#">取消关注</a>
+                  <span>199个回答</span>
+                  <span>1000个关注</span>
+                </div>
+              </div>
+            </div>
+            
+            <div class="follow-section-item">
+              <span class="follow-vote-count pull-left">
+                <div class="follow-vote-num">123</div>
+                <div class="follow-vote-type">浏览</div>
+              </span>
+              <div class="follow-section-main">
+                <h5 class="follow-question"><a href="#">去哪可以买到不贵又体面的家居装饰画呢？</a></h5>
+                <div class="follow-meta">
+                  <a href="#">取消关注</a>
+                  <span>199个回答</span>
+                  <span>1000个关注</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="follow-section-item">
+              <span class="follow-vote-count pull-left">
+                <div class="follow-vote-num">123</div>
+                <div class="follow-vote-type">浏览</div>
+              </span>
+              <div class="follow-section-main">
+                <h5 class="follow-question"><a href="#">去哪可以买到不贵又体面的家居装饰画呢？</a></h5>
+                <div class="follow-meta">
+                  <a href="#">取消关注</a>
+                  <span>199个回答</span>
+                  <span>1000个关注</span>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <ul class="list-group side-nav-group">
+          <li class="list-group-item"><span class="glyphicon glyphicon-list-alt"></span><a href="/BBS/index.php/User/Question/index_all">所有问题</a></li>
+          <li class="list-group-item"><span class="glyphicon glyphicon-th-large"></span><a href="/BBS/index.php/User/Topics/index">话题广场</a></li>
+          <li class="list-group-item"><span class="glyphicon glyphicon-star-empty"></span><a href="#">我的收藏</a></li>
+          <li class="list-group-item"><span class="glyphicon glyphicon-heart"></span><a href="#">我关注的问题</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-1"></div>
+</div>
